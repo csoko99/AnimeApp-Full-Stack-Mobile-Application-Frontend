49729844167fbd103ff6bcbd46161c0337c5c2fb
tanulo <tanulo@barossgabor.local>
2022-01-04 13:07:49 +0100

    szavazat1

--- App.js
+++ App.js
@@ -3,12 +3,19 @@ import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import Anime from './Anime';
-import Mufaj from './Mufaj'
-import Megjelenes from './Megjelenes'
+import Mufaj from './Mufaj';
+import Megjelenes from './Megjelenes';
+import Szavazas from './Szavazas'
 
 function Animeklap({ navigation }) {
   return (
     <Anime/>
+  );
+}
+
+function Szavazaslap({ navigation }) {
+  return (
+    <Szavazas/>
   );
 }
 
@@ -33,6 +40,7 @@ export default function App() {
         <Drawer.Screen name="Animék" component={Animeklap} />
         <Drawer.Screen name="Műfaj" component={Mufajlap} />
         <Drawer.Screen name="Megjelenés" component={Megjeleneslap} />
+        <Drawer.Screen name="Szavazás" component={Szavazaslap} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
